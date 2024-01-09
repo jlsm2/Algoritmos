e834c38dba8833475cedcd5caa2ca82d703d3075
João Marrocos <135621408+jlsm2@users.noreply.github.com>
2024-01-09 14:35:27 -0300

    Update q2.cpp

--- Lista 3/q2.cpp
+++ Lista 3/q2.cpp
@@ -17,7 +17,7 @@ struct fila {
     assentos** table;
 };
 
-ocupantes* criar(string nome; int prioridade; int numRegistro) {
+ocupantes* criar(string nome, int prioridade, int numRegistro) {
     ocupantes* newNode = new ocupantes;
     newNode->nome = nome;
     newNode->prioridade = prioridade;
@@ -25,10 +25,10 @@ ocupantes* criar(string nome; int prioridade; int numRegistro) {
     return newNode;
 }
 
-int hashFunction(fila* hashtable, int numFileira, int numAssentos) {
+int hashFunction(fila* fila, int numFileira, int numAssentos) {
     for(int i=0;i<numFileira;i++) {
         for(int j=0;j<numAssentos;j++) {
-            if(hashtable->table[i]->heap[j] == nullptr) {
+            if(fila->table[i]->heap[j] == nullptr) {
                 return i;
             }
         }
@@ -56,132 +56,256 @@ void maxheapify(ocupantes* arr[], int n, int i) {
     }
 }
 
+void minheapify(ocupantes* arr[], int n, int i) {
+    int smallest = i;
+    int l = 2*i + 1;
+    int r = 2*i + 2;
+    
+    if(r < n && arr[r]->prioridade < arr[smallest]->prioridade) {
+        smallest = r;
+    }
+ 
+    if(l < n && arr[l]->prioridade < arr[smallest]->prioridade) {
+        smallest = l;
+    }
+
+    if(smallest != i) {
+        swap(arr[i], arr[smallest]);
+        maxheapify(arr, n, smallest);
+    }
+}
+
+void buildmaxheap(ocupantes* arr[], int n) {
+    if(n>=2) {
+        for(int i=(n/2) - 1;i>=0;i--) {
+            maxheapify(arr, n, i);
+        }
+    }
+}
+
+void buildminheap(ocupantes* arr[], int n) {
+    if(n>=2) {
+        for(int i=(n/2) - 1;i>=0;i--) {
+            minheapify(arr, n, i);
+        }
+    }
+}
+
 void insertmaxheap(assentos* assento, ocupantes* ocupante) {
     assento->heap[assento->value] = ocupante;
     ++assento->value;
     buildmaxheap(assento->heap, assento->value);
 }
 
-void buildmaxheap(ocupantes* arr[], int n) {
-    if(n>=2) {
-        for(int i=(n/2) - 1; i>=0;i--) {
-            maxheapify(arr, n, i);
-        }
+void insertminheap(assentos* assento, ocupantes* ocupante) {
+    if(assento->heap[assento->value-1] == nullptr) {
+        assento->heap[assento->value-1] = ocupante;
     }
+    else {
+        assento->heap[assento->value] = ocupante;
+        ++assento->value;
+    }
+    buildminheap(assento->heap, assento->value);
 }
+
+ocupantes* removemaxheap(assentos* assento) {
+    if(assento->value == 1) {
+        assento->value--;
+        return assento->heap[0];
+    }
+
+    ocupantes* root = assento->heap[0];
+    assento->heap[0] = assento->heap[assento->value-1];
+    assento->value--;
+
+    maxheapify(assento->heap, assento->value, 0);
+
+    return root;
+}
+
+ocupantes* removeminheap(assentos* assento) {
+    if(assento->value == 1) {
+        assento->value--;
+        return assento->heap[0];
+    }
+
+    ocupantes* root = assento->heap[0];
+    assento->heap[0] = assento->heap[assento->value-1];
+    assento->value--;
+
+    minheapify(assento->heap, assento->value, 0);
+
+    return root;
+}
+
 
 // não mudar ate aqui
 // mudar antes daqui ta tudo certo
 
-void cadastrar(fileiras* head1, assentos* head2, string name, int priority, int numCadastro) {
-    if (head2 == nullptr && head1 == nullptr) {
-        head2->value = name;
-        head2->priority = priority;
-        head2->fil = i;
-        head2->sentada = true;
-        head1->cadeirasOcupadas+=1;
+void cadastrar(int numFileiras, int numAssentos, ocupantes* ocupante, fila* fila, assentos* espera) {
+    int numFileira = hashFunction(fila, numFileiras, numAssentos);
+
+    if(numFileira == -1) {
+        int fileiraMenor = -1;
+        ocupantes* ocupanteMenor = nullptr;
+
+        for(int i = 0; i < numFileiras; i++) {
+            ocupantes* atual = fila->table[i]->heap[0];
+            if(ocupanteMenor == nullptr || atual->prioridade < ocupanteMenor->prioridade) {
+                ocupanteMenor = atual;
+                fileiraMenor = i;
+            }
+        }
+        ocupantes* atual = removeminheap(fila->table[fileiraMenor]);
+
+        if(atual->prioridade > ocupante->prioridade) {
+            insertmaxheap(espera, ocupante); 
+            insertminheap(fila->table[fileiraMenor], atual); 
+            cout << ocupante->nome << " (" << ocupante->numRegistro << ") nao foi alocado(a) em nenhuma fileira" << "\n";
+            
+        }
+        else if(atual->prioridade < ocupante->prioridade) {
+            insertmaxheap(espera, atual);
+            insertminheap(fila->table[fileiraMenor], ocupante);
+
+            cout << ocupante->nome << " (" << ocupante->numRegistro << ") foi alocado(a) na fileira " << fileiraMenor + 1 << "\n";
+
+        }
+        else {
+            if(atual->numRegistro < ocupante->numRegistro) {
+                insertmaxheap(espera, ocupante);
+                insertminheap(fila->table[fileiraMenor], atual); 
+
+                cout << ocupante->nome << " (" << ocupante->numRegistro << ") nao foi alocado(a) em nenhuma fileira" << "\n";
+
+            }
+            else {
+                insertmaxheap(espera, atual);
+                insertminheap(fila->table[fileiraMenor], ocupante);
+
+                cout << ocupante->nome << " (" << ocupante->numRegistro << ") foi alocado(a) na fileira " << fileiraMenor + 1 << "\n";
+
+            }
+        }
     }
     else {
-        while(head1 != nullptr) {
-            while(head2 != nullptr) {
-                if(head2->sentada) {
-                    head2 = head2->next;
-                }
-                else {
-                    head2->value = name;
-                    head2->priority = priority;
-                    head2->fil = i;
-                    head2->sentada = true;
-                    head1->cadeirasOcupadas+=1;
-                    alocada = true;
+        cout << ocupante->nome << " (" << ocupante->numRegistro << ") foi alocado(a) na fileira " << numFileira + 1 << "\n";
+        insertminheap(fila->table[numFileira], ocupante);
+    }
+}
+
+void ver(int numFileiras, int numAssentos, int registro, string nome, fila* fila, assentos* espera) {
+    bool inexistente = true;
+    bool semAssento = false;
+    for(int i = 0; i < numFileiras; i++) {
+        for(int j = 0; j < numAssentos; j++) {
+            if(fila->table[i]->heap[j] != nullptr) {
+                if(fila->table[i]->heap[j]->numRegistro == registro && fila->table[i]->heap[j]->nome == nome) {
+                    cout << "Sentado(a) na fileira " << i + 1 << "\n";
+                    inexistente = false;
+                    semAssento = true;
                     break;
                 }
             }
-            if(alocada) {
+        }
+    }
+
+    if(!semAssento) {
+        for(int i = 0; i < espera->value; i++) {
+            if(espera->heap[i]->numRegistro == registro && espera->heap[i]->nome == nome) {
+                cout << "Sem assento" << "\n";
+                inexistente = false;
                 break;
-            }
-            else {
-                i++;
             }
         }
     }
-    if(alocada) {
-        cout << name << " (" << numCadastro << ") " << "foi alocado(a) na fileira " << head2->fil << "\n";
+    
+    if(inexistente) {
+        cout << "Inexistente" << "\n";
+    }
+}
+
+void remover(int numFileiras, int numAssentos, int registro, string nome, fila* fila, assentos* espera) {
+    ocupantes* procurando = nullptr;
+    int numFileira = -1;
+    for(int i = 0; i < numFileiras; i++) {
+        for(int j = 0; j < numAssentos; j++) {
+            if(fila->table[i]->heap[j] != nullptr) {
+                if(fila->table[i]->heap[j]->numRegistro == registro && fila->table[i]->heap[j]->nome == nome) {
+                    procurando = fila->table[i]->heap[j];
+                    fila->table[i]->heap[j] = fila->table[i]->heap[fila->table[i]->value-1];
+                    fila->table[i]->heap[fila->table[i]->value-1] = procurando;
+                    fila->table[i]->heap[fila->table[i]->value-1] = nullptr;
+                    numFileira = i;
+                    fila->table[i]->value--;
+                }
+            }
+        }
+    }
+
+    if(procurando == nullptr) {
+        if(procurando == nullptr) {
+            for(int i = 0; i < espera->value; i++) {
+                if(espera->heap[i]->numRegistro == registro && espera->heap[i]->nome == nome) {
+                    procurando = espera->heap[i];
+                    numFileira = i;
+                }
+            }
+        }
+        else {
+            if(espera->value != 0) {
+                ocupantes* removido = removemaxheap(espera);
+            }
+        }
     }
     else {
-        cout << name << " (" << numCadastro << ") " << "nao foi alocado(a) em nenhuma fileira" << "\n";
+        if(espera->value != 0) {
+            ocupantes* naFila = removemaxheap(espera);
+            insertminheap(fila->table[numFileira], naFila);
+        }
     }
-}
 
-void ver(fileiras* head1, assentos* head2, string name) {
-    bool achou = false;
-    while(head1 != nullptr) {
-        while(head2 != nullptr) {
-            if(head2->value == name) {
-                cout << "Sentado(a) na fileira " << head2->fil << "\n";
-                achou = true;
-                break;
-            }
-            else {
-                head2 = head2->next;
-            }
-        }
-        if(achou) {
-            break;
-        }
-    }
-    if(!achou) {
+    if(procurando == nullptr) {
         cout << "Inexistente" << "\n";
     }
-}
-
-void remover(fileiras* head1, assentos* head2, string name) {
-    bool removeu = false;
-    while(head1 != nullptr) {
-        while(head2 != nullptr) {
-            if(head2->value == name) {
-                cout << "Removido(a)" << "\n";
-                removeu = true;
-                head2->value = "";
-                head2->sentada = false;
-                head2->priority = 0;
-                break;
-            }
-            else {
-                head2 = head2->next;
-            }
-        }
-        if(removeu) {
-            break;
-        }
-    }
-    if(!removeu) {
-        cout << "Inexistente" << "\n";
+    else {
+        cout << "Removido(a)" << "\n";
     }
 }
 
 int main() {
-    int numFileiras, numAssentos, n, p, numCadastro = 1;
+    int numFileiras, numAssentos, n, prioridade, numCadastro = 1;
     string command, name;
     cin >> numFileiras >> numAssentos >> n;
     
-    fileiras* head1 = nullptr;
-    assentos* head2 = nullptr;
-    
-    fileiras* totalFileiras = criarFileiras(numFileiras);
-    assentos* totalAssentos = criarAssentos(numAssentos);
+    fila* filas = new fila;
+    filas->value = numFileiras;
+    filas->table = new assentos*[numFileiras-1];
+
+    for(int i = 0; i < numFileiras; i++) {
+        filas->table[i] = new assentos;
+        filas->table[i]->value = 0;
+        filas->table[i]->heap = new ocupantes*[numAssentos];
+        for (int j = 0; j < numAssentos; j++) {
+            filas->table[i]->heap[j] = nullptr;
+        }
+    }
+    assentos* espera = new assentos;
+    espera->value = 0;
+    espera->heap = new ocupantes*[2*(numAssentos*numFileiras)];
     
     for(int i=0;i<n;i++) {
-        cin >> command >> name >> p;
+        cin >> command >> name >> prioridade;
         if(command == "CAD") {
-            cadastrar(head1, head2, name, p, numCadastro);
-            numCadastro+=1;
+            ocupantes* ocupante = criar(name, numCadastro, prioridade);
+            cadastrar(numFileiras, numAssentos, ocupante, filas, espera);
+            numCadastro++;
         }
         else if (command == "VER") {
-            ver(head1, head2, name);
+            ver(numFileiras, numAssentos, numCadastro, name, filas, espera);
         }
         else if (command == "REM") {
-            remover(head1, head2, name);
+            remover(numFileiras, numAssentos, numCadastro, name, filas, espera);
         }
     }
     
